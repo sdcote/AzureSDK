468135a5c25e0a23ece2ebaf68e8c7338c964192
Steve Cote <sdcote@gmail.com>
2016-07-21 13:58:02 -0400

    javadoc

--- src/test/java/org/eclipse/paho/client/mqttv3/MqttCallback.java
+++ src/test/java/org/eclipse/paho/client/mqttv3/MqttCallback.java
@@ -1,5 +1,11 @@
 package org.eclipse.paho.client.mqttv3;
 
+/**
+ * Used to intercept the setting made by the SDK when using Paho.
+ * To use, remove the Paho JAR from the classpath and move this and other 
+ * classes to the main code directory. The SDK will load these instead of the 
+ * Paho classes and provide you the ability to see what the SDK sets.
+ */
 public interface MqttCallback {
 
 }
